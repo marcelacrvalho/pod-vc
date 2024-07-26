344a74e6d44279e3e77302f4664e6e4ecfaf42cd
marcelacrvalho <marcelacrv@icloud.com>
2024-07-25 21:01:53 -0300

    feature/002: Inicia o layout da tela Home

--- lib/home/presenter/screens/home_screen.dart
+++ lib/home/presenter/screens/home_screen.dart
@@ -1,27 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
+    return Scaffold(
+      body: SafeArea(
         child: Column(
           children: [
-            Flexible(
-              flex: 2,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color(0xffFAD26D),
-                  borderRadius: BorderRadius.circular(10.0),
+            Container(
+              width: double.infinity,
+              height: MediaQuery.sizeOf(context).height * 0.1,
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Image.asset('assets/images/pod.vc.png'),
+                    Flexible(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('home'),
+                          Text('dashboard'),
+                          Text('contato'),
+                          Text('ser um criador'),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xffFAD26D),
+              ),
+              child: Flexible(
                 child: Row(
                   children: [
-                    Text(
-                        'Seu apoio é fundamental para fazer seus podcasts preferidos\ncrescerem cada vez mais'),
-                    Image.asset('assets/images/girl.jpg'),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                            'Seu apoio é fundamental para fazer seus podcasts preferidos crescerem cada vez mais'),
+                      ),
+                    ),
+                    Flexible(
+                      flex: 2,
+                      child: Image.asset(
+                        'assets/images/girl.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ],
                 ),
               ),
